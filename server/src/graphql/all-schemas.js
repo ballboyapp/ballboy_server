6559bf89e3fc7cc56c1b610c45565896efd67fd9
federodes <federodes@gmail.com>
2019-07-03 17:15:02 +0200

    distance in km

--- server/src/graphql/all-schemas.js
+++ server/src/graphql/all-schemas.js
@@ -1,11 +1,13 @@
 const Base = require('./base');
 const User = require('./user');
+const Spot = require('./spot');
 const Subscription = require('./subscription');
 
 // Add all your schemas here!
 const allSchemas = {
   Base,
   User,
+  Spot,
   Subscription,
 };
 
