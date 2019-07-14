c90849372bbf644929ce0a952d82f34f949cf0e4
federodes <federodes@gmail.com>
2019-07-14 13:46:52 +0200

    fix bug on attendee resolver

--- server/src/graphql/activity/resolvers/activity/attendees.js
+++ server/src/graphql/activity/resolvers/activity/attendees.js
@@ -1,6 +1,6 @@
 const attendees = (root, args, ctx) => {
   // console.log('attendeesField', root, args);
-  return ctx.User.getPublicUsers({ _ids: root.attendeesIds });
+  return ctx.models.User.getPublicUsers({ _ids: root.attendeesIds });
 };
 
 module.exports = attendees;
