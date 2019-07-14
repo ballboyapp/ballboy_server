e5b18754523e49ae40a4d4eabb8d89e7edae2c56
federodes <federodes@gmail.com>
2019-07-14 13:51:40 +0200

    exposing isAttendee resolver

--- server/src/graphql/activity/resolvers/activity/index.js
+++ server/src/graphql/activity/resolvers/activity/index.js
@@ -2,12 +2,14 @@ const organizer = require('./organizer');
 const spot = require('./spot');
 const chatkitRoomId = require('./chatkit-room-id');
 const attendees = require('./attendees');
+const isAttendee = require('./is-attendee');
 
 const Activity = {
   organizer,
   spot,
   chatkitRoomId,
   attendees,
+  isAttendee,
 };
 
 module.exports = Activity;
