204616a6433b02a09083af5c5b5bcebbec84438d
federodes <federodes@gmail.com>
2019-07-17 16:22:12 +0200

    chatkit working :)

--- server/src/startup/chatkit-auth.js
+++ server/src/startup/chatkit-auth.js
@@ -1,10 +1,13 @@
 const chatkit = require('../services/chatkit');
 
+const { CHATKIT_USER_ID } = process.env;
+
 module.exports = (app) => {
   app.post('/chatkit-auth', (req, res) => {
     // User data is decoded on the validateJwtMiddleware
-    const userId = (req.user && req.user._id) || null;
+    const userId = (req.user && req.user._id) || CHATKIT_USER_ID;
     const authData = chatkit.authenticate({ userId });
+    // console.log('authData', authData);
     res.status(authData.status).send(authData.body); // { access_token, expires_in }
   });
 };
