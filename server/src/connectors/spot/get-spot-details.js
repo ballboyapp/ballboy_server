07ef9905ba63e2b8d5409b5389e03001fd4d705c
federodes <federodes@gmail.com>
2019-07-29 23:42:07 +0200

    update chatkit user profile on user update

--- server/src/connectors/spot/get-spot-details.js
+++ server/src/connectors/spot/get-spot-details.js
@@ -1,16 +1,38 @@
 const { Spot } = require('../../models');
 
-const getSpotDetails = ({ usr }, { _id }) => {
+const getSpotDetails = async ({ usr }, { _id }) => {
   // Make sure user is logged in
-  // if (!usr || !usr._id || !_id) {
-  //   return null;
-  // }
-
-  if (!_id) {
+  if (!usr || !usr._id || !_id) {
     return null;
   }
 
+  console.log('usr', usr, '_id', _id);
+
   return Spot.findOne({ _id });
+
+  // const pipeline = [
+  //   {
+  //     $geoNear: {
+  //       near: {
+  //         type: 'Point',
+  //         coordinates: usr.location.coordinates,
+  //       },
+  //       query: { _id },
+  //       // maxDistance
+  //       spherical: true,
+  //       distanceField: 'distance', // attaches a 'distance' (meters) field to the doc
+  //     },
+  //   },
+  //   {
+  //     $limit: 1,
+  //   },
+  // ];
+
+  // console.log('AGGREGATION', JSON.stringify(await Spot.aggregate(pipeline)));
+
+  // const spots = await Spot.aggregate(pipeline);
+
+  // return spots && spots.length > 0 ? spots[0] : null;
 };
 
 module.exports = getSpotDetails;
--- server/src/graphql/user/resolvers/mutation/update-user.js
+++ server/src/graphql/user/resolvers/mutation/update-user.js
@@ -1,7 +1,25 @@
-const updateUser = (root, args, ctx) => {
+const chatkit = require('../../../../services/chatkit');
+
+const updateUser = async (root, args, ctx) => {
   console.log('updateUserMutation', args);
-  return ctx.models.User.updateUser(args);
-  // TODO: update chatkit profile
+  const user = await ctx.models.User.updateUser(args);
+
+  // Update chatkit profile
+  const { username, avatar } = user.profile;
+
+  if (username && avatar) {
+    try {
+      await chatkit.updateUser({
+        id: user._id.toString(),
+        name: username,
+        avatarURL: avatar,
+      });
+    } catch (exc) {
+      console.log('Failed updating user on Chatkit', exc);
+    }
+  }
+
+  return user;
 };
 
 module.exports = updateUser;
