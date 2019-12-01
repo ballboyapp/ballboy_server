ab0d6b07422f93f71b0e31d9a27ec0abbfc7dff0
federodes <federodes@gmail.com>
2019-12-01 13:28:12 +0100

    remove img from cloudinary after avatar update

--- src/graphql/user/resolvers/mutation/update-user.js
+++ src/graphql/user/resolvers/mutation/update-user.js
@@ -1,25 +1,46 @@
 const chatkit = require('../../../../services/chatkit');
+const { imgUploader } = require('../../../../services/img-uploader');
 
 const updateUser = async (root, args, ctx) => {
   console.log('updateUserMutation', args);
-  const user = await ctx.models.User.updateUser(args);
+  const { usr } = ctx;
+
+  if (!usr) {
+    return null;
+  }
+
+  const upUser = await ctx.models.User.updateUser(args);
 
   // Update chatkit profile
-  const { username, avatar } = user.profile;
+  const {
+    username: newUsername,
+    avatar: newAvatar,
+  } = upUser.profile;
 
-  if (username && avatar) {
+  if (newUsername && newAvatar) {
     try {
       await chatkit.updateUser({
-        id: user._id.toString(),
-        name: username,
-        avatarURL: avatar,
+        id: usr._id.toString(),
+        name: newUsername,
+        avatarURL: newAvatar,
       });
     } catch (exc) {
       console.log('Failed updating user on Chatkit', exc);
     }
   }
 
-  return user;
+  // Remove old avatar from cloudinary
+  const { avatar: oldAvatar } = usr.profile;
+
+  if (newAvatar !== oldAvatar) {
+    try {
+      await imgUploader.deleteImg(oldAvatar);
+    } catch (exc) {
+      console.log('Failed removing old image resource', exc);
+    }
+  }
+
+  return upUser;
 };
 
 module.exports = updateUser;
